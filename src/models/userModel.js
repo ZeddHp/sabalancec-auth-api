ee969576128a2a36516f867566c3173fc3b595b7
probablyalright <112282114+probablyalright@users.noreply.github.com>
2025-03-10 16:59:49 +0200

    Add mongoose model schemas

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,5 +1,16 @@
-const Datastore = require('nedb-promises');
+const mongoose = require('mongoose');
 
-const users = Datastore.create({ filename: 'Users.db', autoload: true });
+//const users = Datastore.create({ filename: 'Users.db', autoload: true });
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    fullName: String,       //"Valters Jargans"
+    email: String,          //"valters.walle@gmail.com"
+    address: String,        //"Zvejnieku iela 13"
+    password: String,       //"$2b$12$1CoqZ75FaqryXODle4dYbet3QbKBobyB3QFtawtA0ED1uYIcSa6ua"
+});
+
+const users = mongoose.model("users", userSchema);
 
 module.exports = { users };
